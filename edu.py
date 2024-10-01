b1d5a1fa6a15f1b16045e606caed733a245c20cc
Mark <markbakalkinsochi3@gmail.com>
2024-10-01 18:13:46 +0300

    First function

--- edu.py
+++ edu.py
@@ -1,2 +1,13 @@
-print('Hello world')
-print('Hello world')
+def count_food():
+    fruits = int(input('Количество фруктов: '))
+    veg = int(input('Количество овощей: '))
+    print(f'Всего - {fruits + veg}')
+    
+print('Обезьяны')
+count_food()
+
+print('\nЖирафы')
+count_food()
+
+print('\nСлоны')
+count_food()
