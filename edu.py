8ca00221c5f1313fb5ef64a3863951703c8bd356
Mark <markbakalkinsochi3@gmail.com>
2024-10-01 18:26:13 +0300

    Second function

--- edu.py
+++ edu.py
@@ -1,13 +1,18 @@
-def count_food():
-    fruits = int(input('Количество фруктов: '))
-    veg = int(input('Количество овощей: '))
-    print(f'Всего - {fruits + veg}')
-    
-print('Обезьяны')
-count_food()
+def triangle():
+    for i in range(1, 6):
+        count_spaces = (5 - i) * 2
+        print((5 - i) * ' ', (9 - count_spaces) * '*', (5 - i) * ' ', sep='')
 
-print('\nЖирафы')
-count_food()
+def rectangle():
+    for i in range(1, 6):
+        if i == 1 or i == 5:
+            print(5 * '*')
+        else:
+            print('*', ' ' * 3, '*', sep='')
 
-print('\nСлоны')
-count_food()
+
+num = int(input('Введите число: '))
+if num == 1:
+    triangle()
+else:
+    rectangle()
