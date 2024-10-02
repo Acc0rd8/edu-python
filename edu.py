a8b892612c3c86afaec98c2a9dbeff149c12978d
Mark <markbakalkinsochi3@gmail.com>
2024-10-02 18:14:44 +0300

    Task1

--- edu.py
+++ edu.py
@@ -1,1 +1,12 @@
-print('Hello world!')
+books_id = [50, 34, -1, -1, 2, 23, -1]
+new_books_id = []
+
+for i in range(10):
+    id = int(input('Введите айди книги: '))
+    books_id.append(id)
+
+for i in books_id:
+    if i != -1:
+        new_books_id.append(i)
+        
+print(new_books_id)
